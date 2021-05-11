29f48a2daa7ccc776800a60c68c5ee0305f0e6c4
muhajirmuhajir <hajir.nf@gmail.com>
2021-05-11 13:16:18 +0800

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6313e2cb49c65d65a59dd383025d450f'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '185e7a72-e311-46e9-99c1-3050a74cb0ed'
+};
